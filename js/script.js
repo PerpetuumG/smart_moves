0c50c3cfc0c15cb0a64e4624b976c5748449ab29
PerpetuumG <perpetuumg@mail.ru>
2020-08-19 12:13:49 +0300

    Анимация линий блока прогресса

--- js/script.js
+++ js/script.js
@@ -8,4 +8,28 @@ $(document).ready(function () {
         $(this).parents('.works__element').removeClass('works__element--active');
     });
 
+    function moveProgressBar(node, nodeLine, tooltip, animationLength = 1500) {
+        const progressElement = $(node);
+        progressElement.each(function (value, item) {
+            $(item).find(nodeLine).animate({
+                width: item.dataset.progressPercent+'%'
+            }, animationLength);
+            $(item).find(tooltip).show(animationLength);
+        });
+    }
+
+    let animate = true;
+
+
+    $(window).scroll(function () {
+
+        if($('.skills').offset().top <= $(window).scrollTop() + 150) {
+            if(animate) {
+                moveProgressBar('.progress__element', '.progress__line', '.progress__tooltip');
+            }
+            animate = false;
+        }
+
+    });
+
 });
